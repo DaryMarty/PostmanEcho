9d5a59799954a421037a69bf735f9f6bba2d602d
Darya Martemyanova <mdy.89@mail.ru>
2022-01-29 16:23:48 +0300

    Success

--- src/test/java/ru/netology/rest/MobileBankApiTestV7.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV7.java
@@ -10,15 +10,13 @@ public class MobileBankApiTestV7 {
     @Test
     void shouldSubmitRequest() {
         given()
-                .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
-                .when()
-                .post("/post")
-// Проверки
-                .then()
-                .statusCode(200)
-                .body("data", equalTo("some value"))
+           .baseUri("https://postman-echo.com")
+           .body("some data")
+        .when()
+           .post("/post")
+        .then()
+           .statusCode(200)
+           .body("data", equalTo("some data"))
         ;
     }
 
